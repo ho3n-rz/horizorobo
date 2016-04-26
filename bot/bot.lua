d1a437e2aea69d0f073bd8c1054e11811a07e6c2
horizonrz <hossienrezaei1380@gmail.com>
2016-04-26 11:07:21 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -219,7 +219,7 @@ function create_config( )
     "download_media",
     "invite"
     },
-    sudo_users = {193248161},--Sudo users
+    sudo_users = {219587559},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
